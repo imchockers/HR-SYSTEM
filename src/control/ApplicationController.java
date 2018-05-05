579b7a1f1365511131edf7175f9fc4a7b086349f
Lachlan Clulow <lachlanclulow@gmail.com>
2018-05-05 17:39:18 +1000

    removed debug message

--- src/control/ApplicationController.java
+++ src/control/ApplicationController.java
@@ -6,7 +6,6 @@ public class ApplicationController {
 		while (true) {
 		
 			StaffController controller = StaffController.logIn();
-			System.out.println("Logged In as: " + controller.getID() + ", " + controller.getClass());
 			
 			controller.getCommands();
 		}
