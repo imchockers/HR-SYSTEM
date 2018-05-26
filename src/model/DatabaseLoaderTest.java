acc720eaba79fe1752b78df2579feec90c6be3ca
imchockers <s3682356@student.rmit.edu.au>
2018-05-26 20:50:45 +1000

    added test cases for DatabaseLoader

--- src/model/DatabaseLoaderTest.java
+++ src/model/DatabaseLoaderTest.java
@@ -10,6 +10,7 @@ public class DatabaseLoaderTest {
 	MasterTimetable tt;
 	StaffDatabase staffDb;
 	
+	/**	Tests that the database can load both staff and timetable data	*/
 	@Before
 	public void setUp() throws Exception {
 		tt = DatabaseLoader.loadTimetable();
@@ -18,19 +19,38 @@ public class DatabaseLoaderTest {
 		assertNotNull(staffDb);
 	}
 
+	/**	Tests that DatabaseLoader fails as expected with the incorrect filepath	*/
 	@Test (expected=FileNotFoundException.class)
-	public void testLoadTimetable() throws FileNotFoundException {
+	public void testLoadTimetableFail() throws FileNotFoundException {
 		DatabaseLoader.loadTimetable("pathtonothing");
 	}
+	
+	/**	Tests that DatabaseLoader behaves as expected with the correct filepath	*/
+	@Test
+	public void testLoadTimetable() throws FileNotFoundException {
+		assertNotNull(DatabaseLoader.loadTimetable("timetable.data"));
+	}
 
+	/**	Tests that DatabaseLoader fails as expected with the incorrect filepath	*/
 	@Test (expected=FileNotFoundException.class)
-	public void testLoadStaff() throws FileNotFoundException {
+	public void testLoadStaffFail() throws FileNotFoundException {
 		DatabaseLoader.loadStaff("pathtonothing");
 	}
 	
+	/**	Tests that DatabaseLoader behaves as expected with the correct filepath	*/
+	@Test
+	public void testLoadStaff() throws FileNotFoundException {
+		assertNotNull(DatabaseLoader.loadStaff("login.data"));
+	}
+	
+	/**	Tests that DatabaseLoader behaves as when saving data to file	*/
 	@After
 	public void tearDown() throws Exception {
-		DatabaseLoader.save(tt.export(), staffDb.export());
+		String retStr= DatabaseLoader.save(tt.export(), staffDb.export());
+		
+		assertArrayEquals(
+				new String("Timetable data saved correctly! Staff data saved correctly!").toCharArray(),
+				retStr.toCharArray());
 	}
 
 }
